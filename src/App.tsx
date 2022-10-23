0776246981a7089275263dd5b7ae33d17ef29b0e
Stephanie Guevara <stephanie.guevara@kairosds.com>
2022-10-23 13:54:11 +0200

    feat: adding initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./ui/views/Home/Home";
 
 const App = () => {
   return (
-    <div> Podcast </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/ui/views/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Podcast</div>;
+};
+
+export default Home;
